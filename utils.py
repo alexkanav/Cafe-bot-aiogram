39b4475890cc5f687d015dad699134be4f481164
alex <sserinua@gmail.com>
2025-06-23 10:32:07 +0300

    Refactor: improve code quality.

--- utils.py
+++ utils.py
@@ -2,19 +2,17 @@ from aiogram.types import BotCommand
 import aiohttp
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from config import TELEGRAM_TOKEN, TELEGRAM_CHANNEL_ID
-
 
 class MongoDB:
-    def __init__(self, uri: str):
+    def __init__(self, uri: str, menu_collection=None, orders_collection=None):
         self.menu = []
         self.client = AsyncIOMotorClient(uri)
         self.db = self.client["cafe"]
-        self.menu_collection = self.db["menu"]
-        self.orders_collection = self.db["orders"]
+        self.menu_collection = menu_collection or self.db["menu"]
+        self.orders_collection = orders_collection or self.db["orders"]
 
-    async def get_menu(self):
-        cursor = self.menu_collection.find()
+    async def get_menu(self, cursor=None):
+        cursor = cursor or self.menu_collection.find()
         self.menu = await cursor.to_list(length=None)
 
     async def update_menu(self, category: str, item: str, values: list[str]) -> str:
@@ -39,9 +37,9 @@ async def set_commands(bot):
     await bot.set_my_commands(commands)
 
 
-async def send_to_telegram(text: str):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {"chat_id": TELEGRAM_CHANNEL_ID, "text": text}
+async def send_to_telegram(text: str, telegram_token: str, telegram_channel_id: str):
+    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
+    payload = {"chat_id": telegram_channel_id, "text": text}
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=payload) as response:
             if response.status != 200:
